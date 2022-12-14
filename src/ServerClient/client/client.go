78f37e49d7f953d95fcac97b316c62047d686d68
Oddernumse <72282154+Oddernumse@users.noreply.github.com>
2022-12-14 09:00:29 +0000

    Add files via upload

---
+++ src/ServerClient/client/client.go
@@ -0,0 +1,51 @@
+package client
+
+import (
+	"bytes"
+	"encoding/binary"
+	"net"
+	"os"
+)
+
+const (
+	HOST = "DESKTOP-H2H53J5"
+	PORT = "1234"
+	TYPE = "tcp"
+)
+
+func dialTCP() *net.TCPConn {
+	tcpServer, err := net.ResolveTCPAddr(TYPE, HOST+":"+PORT)
+	if err != nil {
+		println("resolveTCPAddr failed:", err.Error())
+		os.Exit(1)
+	}
+
+	conn, err := net.DialTCP(TYPE, nil, tcpServer)
+	if err != nil {
+		println("Dial failed:", err.Error())
+		os.Exit(1)
+	}
+
+	return conn
+}
+
+func sendToTCP(conn net.Conn, payload int32) {
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.LittleEndian, payload)
+	buf.WriteTo(conn)
+	// _, err := conn.Write([]byte(payload))
+	if err != nil {
+		println("Failed to write data:", err.Error())
+		os.Exit(1)
+	}
+}
+
+func receiveFromTCP(conn net.Conn) []byte {
+	received := make([]byte, 1024)
+	_, err := conn.Read(received)
+	if err != nil {
+		println("Read data failed:", err.Error())
+		os.Exit(1)
+	}
+	return received
+}
