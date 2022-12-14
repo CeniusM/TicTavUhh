9445c4cf6d09abb15d2938c407f86068b2ea0cb1
CeniusM <cenius.meier@gmail.com>
2022-12-14 10:50:06 +0100

    Adding loop and thread to server

--- src/Server/Server/TicTavUhhServer.cs
+++ src/Server/Server/TicTavUhhServer.cs
@@ -6,8 +6,9 @@ namespace TicTavUhhServer
 {
     public class Server
     {
-        
+
         public bool Running { get; private set; }
+        public bool StoppingServer { get; private set; }
 
         private ILogger logger;
         public (string Message, LogWarningLevel WarningLevel) GetNextLog() => logger.GetLog();
@@ -16,17 +17,39 @@ namespace TicTavUhhServer
 
         public Server(ILogger logger)
         {
+            this.Running = false;
+            this.StoppingServer = false;
             this.logger = logger;
         }
 
+        /// <summary>
+        /// Starts a new thread the server lives on, if there isent allready a thread running
+        /// </summary>
         public void Start()
         {
+            if (!Running)
+            {
+                Thread thread = new Thread(ServerLoop);
+                thread.Start();
+            }
+        }
 
+        private void ServerLoop()
+        {
+            Running = true;
+
+            while (Running)
+            {
+                if (StoppingServer)
+                    break;
+            }
+
+            Running = false;
         }
 
         public void Stop()
         {
-
+            StoppingServer = true;
         }
     }
 }
