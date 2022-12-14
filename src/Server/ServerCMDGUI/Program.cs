d6d381fdf1aeba84cc2bcd8f8cd214face8a3733
CeniusM <cenius.meier@gmail.com>
2022-12-14 10:50:41 +0100

    Added a new procjet that will be used for starting/stopping the server and disblaying the logs

---
+++ src/Server/ServerCMDGUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using TicTavUhhServer;
+
+Server server = new Server();
+
+server.Start();
+
+
+while (true)
+{
+    var log = server.GetNextLog();
+    Console.ForegroundColor = ConsoleColor.Green;
+    if (log.Message != string.Empty)
+        Console.WriteLine(log.Message);
+    Thread.Sleep(1000);
+}
+
+server.Stop();
+
