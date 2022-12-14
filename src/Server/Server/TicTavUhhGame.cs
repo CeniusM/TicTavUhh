607f9f6cf015a30fbfe8ea845740242a1bb69e21
CeniusM <cenius.meier@gmail.com>
2022-12-14 09:59:33 +0100

    Added empty class game for handeling the game between two players

---
+++ src/Server/Server/TicTavUhhGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace TicTavUhhServer;
+
+internal class TicTavUhhGame
+{
+    internal bool Running { get; private set; }
+
+    public TicTavUhhGame()
+    {
+
+    }
+
+    public void Play(Stream player1, Stream player2)
+    {
+        
+    }
+
+    ~TicTavUhhGame()
+    {
+
+    }
+}
